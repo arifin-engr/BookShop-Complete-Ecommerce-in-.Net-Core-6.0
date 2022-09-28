1ca4361f156fede9f3716d848d76a03aa5904124
ARIFIN <ARIFIN@DESKTOP-B5K8CDM>
2022-09-28 14:45:07 +0600

    Add project files.

---
+++ BookShop.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Models
+{
+    public class Category
+    {
+        public  int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1,100,ErrorMessage ="Rang Should be 1 to 100")]
+        [Display(Name= "Display Order")]
+        public int DisplayOrder { get; set; }
+        public DateTime dateTime { get; set; } = DateTime.Now;
+
+    }
+}
